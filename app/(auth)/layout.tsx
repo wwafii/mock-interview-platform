d0d169767b6409eeda4d42d9da6331195bffb751
Wafiul Achdi <achdiwafiul@gmail.com>
2025-08-05 23:37:53 +0700

    feat(routes): create application routes for navigation

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const AuthLayout = ({ children }: {children: ReactNode }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
